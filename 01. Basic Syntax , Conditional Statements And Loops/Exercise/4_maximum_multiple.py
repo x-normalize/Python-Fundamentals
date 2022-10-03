c6cd69d8272b88420896d722b4d0deb20859d776
Yordan Nikolov <x-normalize@protonmail.com>
2022-10-03 08:48:22 +0300

    create new Task

--- 01. Basic Syntax , Conditional Statements And Loops/Exercise/4_maximum_multiple.py
+++
@@ -1,7 +0,0 @@
-divisor = int(input())
-boundary = int(input())
-for current_numbers in range(boundary, divisor, -1):
-    if current_numbers % divisor == 0:
-        print(current_numbers)
-        break
-
